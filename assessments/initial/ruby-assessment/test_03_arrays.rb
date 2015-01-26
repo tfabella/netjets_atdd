dd7fe412ec4fee6a58ba477b177a56798184cac0
tfabella <tfabella@hotmail.com>
2015-01-26 14:11:38 -0500

    initial assessment

--- assessments/initial/ruby-assessment/test_03_arrays.rb
+++ assessments/initial/ruby-assessment/test_03_arrays.rb
@@ -10,11 +10,11 @@ class AboutArrays < Test::Unit::TestCase
   def test_accessing_array_elements
     array = ['peanut', 'butter', 'and', 'jelly']
 
-    assert_equal __, array[0]
-    assert_equal __, array.first
-    assert_equal __, array[3]
-    assert_equal __, array.last
-    assert_equal __, array[1]
+    assert_equal 'peanut', array[0]
+    assert_equal 'peanut', array.first
+    assert_equal 'jelly', array[3]
+    assert_equal 'jelly', array.last
+    assert_equal 'butter', array[1]
   end
 
 end
